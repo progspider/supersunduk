d618d59abaef555b29c95682e1352e0f8cc2cfb0
progspider <pingvinkin@gmail.com>
2023-12-23 22:11:11 +0600

    1

---
+++ [ЧЕЛОВЕК]/СИСТЕМНЫЙ АДМИНИСТРАТОР/working-scripts/videoedit/merge.bat
@@ -0,0 +1,11 @@
+:: ffmpeg -ss 5707 -t 53 -i source_video.avi -vcodec copy -acodec copy malignant.avi
+
+:: ffmpeg -i video.mp4 -i audio.m4a -c copy output.mp4
+
+:: Superman.and.Lois.S01E01.rus.LostFilm.TV.avi
+
+:: 01:35:07 >> 5707 - 01:36:00 >> 53
+
+
+
+ffmpeg -i video.mp4 -i audio.m4a -c copy output.mp4
---
+++ [ЧЕЛОВЕК]/СИСТЕМНЫЙ АДМИНИСТРАТОР/working-scripts/videoedit/obrezka.bat
@@ -0,0 +1,7 @@
+ffmpeg -ss 2007 -t 164 -i source_video.mkv -vcodec copy -acodec copy La.La.Land.2016.mkv
+
+:: The.Mandalorian.S03E06.rus.LostFilm.TV.avi
+:: YOU.S03E03.rus.LostFilm.TV.avi
+:: 65.2023.D.HDRip.1.46Gb.MegaPeer
+
+:: 00:33:27 >> 9498 - 00:36:11 >> 75   3600   7200
